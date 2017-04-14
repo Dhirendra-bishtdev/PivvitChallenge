0251fab5155bad822752a196a82d85c3c93206cb
dhirendra1123 <dhirendra1123@gmail.com>
2017-04-14 17:37:16 +0530

    Source commit

---
+++ config/web.php
@@ -0,0 +1,77 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+	'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin',
+        ],
+    ],
+    'components' => [
+        'request' => [
+            'cookieValidationKey' => 'hashASV2#4456r666646H$h66xx',
+        ],
+		'paypalpro' => [
+ 
+            'class' => 'app\components\Paypalpro',
+ 
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\Users',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+       
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+			'<module>/<controller>/<action>/<id:\d+>' => '<module>/<controller>/<action>',
+			'<controller>/<action>/<id:\d+>' => '<controller>/<action>',
+            ],
+        ],
+        
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
